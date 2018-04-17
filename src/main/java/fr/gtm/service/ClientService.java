0913971da2ad2f3a8647d10c9f21159a80530a34
douglas <douglas@gtm.com>
2018-04-17 16:00:18 +0200

    Implémentation de ClientDAO avec EntityManager et modification du service pour utiliser le nouveau DAO.

--- src/main/java/fr/gtm/service/ClientService.java
+++ src/main/java/fr/gtm/service/ClientService.java
@@ -1,9 +1,11 @@
 package fr.gtm.service;
+
 import java.util.ArrayList;
+import java.util.List;
 
-import fr.gtm.dao.*;
-import fr.gtm.domaine.*;
-
+import fr.gtm.domaine.Client;
+import fr.gtm.domaine.Conseiller;
+import fr.gtm.jpa.ClientDao;
 
 public class ClientService {
 
@@ -11,53 +13,56 @@ public class ClientService {
 
 	/**
 	 * methode qui nous permet de creer un client
+	 *
 	 * @param leClient
 	 * @return
 	 */
 	public boolean createClient(Client leClient) {
-		boolean reponse = dao.createClient(leClient);
-		return reponse; // retour de la reponse
+		this.dao.create(leClient);
+		return true; // retour de la reponse
+	}
+
+	/**
+	 * methode qui nous permet de supprimer un client
+	 *
+	 * @param leClient
+	 * @return
+	 */
+	public boolean deleteClient(Client leClient) {
+		this.dao.delete(leClient.getIdClient());
+		return true; // retour de la réponse
+	}
+
+	/**
+	 * methode qui nous permet d'afficher une liste de clients
+	 *
+	 * @param c
+	 * @return
+	 */
+	public ArrayList<Client> getAllClient(Conseiller c) {
+		List<Client> listClients = this.dao.getAllClient(c);
+		return (ArrayList<Client>) listClients;
 	}
 
 	/**
 	 * methode qui nous permet d'afficher un client
+	 *
 	 * @param leClient
 	 * @return
 	 */
 	public Client getClient(Client leClient) {
-		leClient = dao.getClient(leClient);
+		leClient = this.dao.read(leClient.getIdClient());
 		return leClient;
 	}
 
 	/**
 	 * methode qui nous permet de mettre a jour un client
+	 *
 	 * @param leClient
 	 * @return
 	 */
 	public Client updateClient(Client leClient) {
-		leClient = dao.updateClient(leClient);
+		this.dao.update(leClient);
 		return leClient;
 	}
-
-	/**
-	 * methode qui nous permet de supprimer un client
-	 * @param leClient
-	 * @return
-	 */
-	public boolean deleteClient(Client leClient) {
-		boolean reponse = dao.deleteClient(leClient);
-		return reponse; // retour de la réponse
-	}
-
-	/**
-	 * methode qui nous permet d'afficher une liste de clients
-	 * @param c
-	 * @return
-	 */
-	public ArrayList<Client> getAllClient(Conseiller c) {
-		ArrayList<Client> listClients = dao.getAllClient(c);
-		return listClients;
-	}
 }
-
-
